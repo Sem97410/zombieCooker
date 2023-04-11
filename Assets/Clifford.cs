36510810aa134326563b1d2b32215a1d61ac1482
Clifford JACCOMARD <cjaccomard@local.iloi.net>
2023-04-11 14:39:19 +0400

    :clifford

---
+++ Assets/Clifford.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clifford : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("coucou les copains <3");
+    }
+
+
+}
