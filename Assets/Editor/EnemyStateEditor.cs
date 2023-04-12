7118c76ee89df8a830ec28813e0ed353f149cca8
Dimitri Mulot <dimitri.mulot974@gmail.com>
2023-04-12 20:44:05 +0400

    RéglagesEditorZombie

--- Assets/Editor/EnemyStateEditor.cs
+++ Assets/Editor/EnemyStateEditor.cs
@@ -22,11 +22,17 @@ public class EnemyStateEditor : Editor
             case Zombie.ZombieState.Guard:
                 {
                     _zombie._target = (Transform)EditorGUILayout.ObjectField("Target", _zombie._target, typeof(Transform), true);
+                    _zombie._radius = (float)EditorGUILayout.FloatField("Radius", _zombie._radius);
+                    _zombie._angle = (float)EditorGUILayout.FloatField("Angle", _zombie._angle);
+                    _zombie._playerRef = (GameObject)EditorGUILayout.ObjectField("Player Ref", _zombie._playerRef, typeof(GameObject), true);
                     break;
                 }
             case Zombie.ZombieState.Waypoint:
                 {
                     _zombie._target = (Transform)EditorGUILayout.ObjectField("Target", _zombie._target, typeof(Transform), true);
+                    _zombie._radius = (float)EditorGUILayout.FloatField("Radius", _zombie._radius);
+                    _zombie._angle = (float)EditorGUILayout.FloatField("Angle", _zombie._angle);
+                    _zombie._playerRef = (GameObject)EditorGUILayout.ObjectField("Player Ref", _zombie._playerRef, typeof(GameObject), true);
                     EditorGUILayout.PropertyField(_zombieWaypoints, new GUIContent("Waypoints"));
                     break;
                 }
