bf9a0b07a6d9fcc03f6c968ae0bb7a835505da6d
Clifford JACCOMARD <cjaccomard@local.iloi.net>
2023-04-12 11:54:03 +0400

    RunInput

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -7,4 +7,20 @@ public class Food : PickUp
     [SerializeField] private float _satiety;
 
     public float Satiety { get => _satiety; set => _satiety = value; }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+    }
+
+    protected override void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+    }
+    public override void PickUpItem()
+    {
+        Debug.Log("Apple");
+        Destroy(gameObject);
+
+    }
 }
