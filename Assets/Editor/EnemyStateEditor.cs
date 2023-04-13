90a1be85e002bb4c2d16c5ca409668ad6a15ee96
Dimitri MULOT <dmulot@local.iloi.net>
2023-04-13 09:27:24 +0400

    EditorEnemyWithoutBug

--- Assets/Editor/EnemyStateEditor.cs
+++ Assets/Editor/EnemyStateEditor.cs
@@ -6,36 +6,87 @@ using UnityEditor;
 [CustomEditor(typeof(Zombie))]
 public class EnemyStateEditor : Editor
 {
-    Zombie _zombie;
-    SerializedProperty _zombieWaypoints;
+    Zombie m_zombie;
+    SerializedProperty m_zombieTarget;
+    SerializedProperty m_zombieRadius;
+    SerializedProperty m_zombieAngle;
+    SerializedProperty m_zombiePlayerRef;
+    SerializedProperty m_zombieWaypoints;
+    SerializedProperty m_zombieWalkRadius;
+    
     private void OnEnable()
     {
-        _zombie = (Zombie)target;
-        _zombieWaypoints = serializedObject.FindProperty("_waypoints");
+        m_zombie = (Zombie)target;
+        m_zombieWaypoints = serializedObject.FindProperty("_waypoints");
+        m_zombieTarget = serializedObject.FindProperty("_target");
+        m_zombieRadius = serializedObject.FindProperty("_radius");
+        m_zombieAngle = serializedObject.FindProperty("_angle");
+        m_zombiePlayerRef = serializedObject.FindProperty("_playerRef");
+        m_zombieWalkRadius = serializedObject.FindProperty("_walkRadius");
     }
 
     public override void OnInspectorGUI()
     {
-        _zombie._zombieState = (Zombie.ZombieState)EditorGUILayout.EnumPopup("Zombie State", _zombie._zombieState);
-        switch (_zombie._zombieState)
+        m_zombie._zombieState = (Zombie.ZombieState)EditorGUILayout.EnumPopup("Zombie State", m_zombie._zombieState);
+        serializedObject.FindProperty("_zombieState").enumValueIndex = (int)m_zombie._zombieState;
+        serializedObject.ApplyModifiedProperties();
+
+        switch (m_zombie._zombieState)
         {
             case Zombie.ZombieState.Guard:
                 {
-                    _zombie._target = (Transform)EditorGUILayout.ObjectField("Target", _zombie._target, typeof(Transform), true);
-                    _zombie._radius = (float)EditorGUILayout.FloatField("Radius", _zombie._radius);
-                    _zombie._angle = (float)EditorGUILayout.FloatField("Angle", _zombie._angle);
-                    _zombie._playerRef = (GameObject)EditorGUILayout.ObjectField("Player Ref", _zombie._playerRef, typeof(GameObject), true);
+                    EditorGUILayout.PropertyField(m_zombieTarget, new GUIContent("Target"));
+                    EditorGUILayout.PropertyField(m_zombieRadius, new GUIContent("Radius"));
+                    EditorGUILayout.Slider(m_zombieAngle, 0, 360, new GUIContent("Angle"));
+                    EditorGUILayout.PropertyField(m_zombiePlayerRef, new GUIContent("PlayerRef"));
+                    serializedObject.ApplyModifiedProperties();
                     break;
                 }
             case Zombie.ZombieState.Waypoint:
                 {
-                    _zombie._target = (Transform)EditorGUILayout.ObjectField("Target", _zombie._target, typeof(Transform), true);
-                    _zombie._radius = (float)EditorGUILayout.FloatField("Radius", _zombie._radius);
-                    _zombie._angle = (float)EditorGUILayout.FloatField("Angle", _zombie._angle);
-                    _zombie._playerRef = (GameObject)EditorGUILayout.ObjectField("Player Ref", _zombie._playerRef, typeof(GameObject), true);
-                    EditorGUILayout.PropertyField(_zombieWaypoints, new GUIContent("Waypoints"));
+                    EditorGUILayout.PropertyField(m_zombieTarget, new GUIContent("Target"));
+                    EditorGUILayout.PropertyField(m_zombieRadius, new GUIContent("Radius"));
+                    EditorGUILayout.Slider(m_zombieAngle, 0, 360, new GUIContent("Angle"));
+                    EditorGUILayout.PropertyField(m_zombiePlayerRef, new GUIContent("PlayerRef"));
+                    EditorGUILayout.PropertyField(m_zombieWaypoints, new GUIContent("Waypoints"));
+                    serializedObject.ApplyModifiedProperties();
+                    break;
+                }
+            case Zombie.ZombieState.Random:
+                {
+                    EditorGUILayout.PropertyField(m_zombieTarget, new GUIContent("Target"));
+                    EditorGUILayout.PropertyField(m_zombieWalkRadius, new GUIContent("Walk Radius"));
+                    EditorGUILayout.PropertyField(m_zombieRadius, new GUIContent("Radius"));
+                    EditorGUILayout.Slider(m_zombieAngle, 0, 360, new GUIContent("Angle"));
+                    EditorGUILayout.PropertyField(m_zombiePlayerRef, new GUIContent("PlayerRef"));
                     break;
                 }
         }
+    }
+
+    private void OnSceneGUI()
+    {
+        Zombie _zombieFOV = (Zombie)target;
+        Handles.color = Color.black;
+        Handles.DrawWireArc(_zombieFOV.transform.position, Vector3.up, Vector3.forward, 360, _zombieFOV._radius);
+
+        Vector3 viewAngle01 = DirectionFromAngle(_zombieFOV.transform.eulerAngles.y, -_zombieFOV._angle / 2);
+        Vector3 viewAngle02 = DirectionFromAngle(_zombieFOV.transform.eulerAngles.y, _zombieFOV._angle / 2);
+
+        Handles.color = Color.yellow;
+        Handles.DrawLine(_zombieFOV.transform.position, _zombieFOV.transform.position + viewAngle01 * _zombieFOV._radius);
+        Handles.DrawLine(_zombieFOV.transform.position, _zombieFOV.transform.position + viewAngle02 * _zombieFOV._radius);
+
+        if (_zombieFOV._canSeePlayer)
+        {
+            Handles.color = Color.green;
+            Handles.DrawLine(_zombieFOV.transform.position, _zombieFOV._playerRef.transform.position);
+        }
+    }
+
+    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
