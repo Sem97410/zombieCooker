8fa843ba10bd554a75f2a142516ab34f0cdf8f4c
Clifford JACCOMARD <cjaccomard@local.iloi.net>
2023-04-13 09:49:44 +0400

    Oven class

--- Assets/Scripts/mainCharacter.cs
+++ Assets/Scripts/mainCharacter.cs
@@ -36,6 +36,7 @@ public class mainCharacter : LivingObject
     public List<PickUp> PickUps { get => _pickUp; set => _pickUp = value; }
     public int ChoixIndex { get => _choixIndex; set => _choixIndex = value; }
     public Transform ItemPos { get => _itemPos; set => _itemPos = value; }
+    public float MovementSpeed { get => _movementSpeed; set => _movementSpeed = value; }
 
     [SerializeField]
     private float _hungerDecreaseRun; // la perte de faim quand on court
